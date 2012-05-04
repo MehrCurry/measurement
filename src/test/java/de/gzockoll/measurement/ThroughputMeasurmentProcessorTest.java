56b06845a9bcec1d781bd59e5e7bcc777321f8c2
Guido Zockoll <gzockoll@gmail.com>
2012-05-04 06:36:25 +0200

    More Tests

---
+++ src/test/java/de/gzockoll/measurement/ThroughputMeasurmentProcessorTest.java
@@ -0,0 +1,41 @@
+package de.gzockoll.measurement;
+
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.gzockoll.measurement.ThroughputMeasurmentProcessor;
+
+public class ThroughputMeasurmentProcessorTest {
+	private ThroughputMeasurmentProcessor bus;
+
+	@Before
+	public void setUp() {
+		bus = new ThroughputMeasurmentProcessor(1000);
+	}
+
+	@Test
+	public void testAdd() {
+		assertThat(bus.getQueueSize(), is(0));
+		bus.add(null);
+		assertThat(bus.getQueueSize(), is(1));
+	}
+
+	@Test
+	public void testGetQueueSize() throws InterruptedException {
+		for (int i = 0; i < 5; i++) {
+			bus.add(null);
+		}
+		Thread.sleep(500);
+		for (int i = 0; i < 5; i++) {
+			bus.add(null);
+		}
+		assertThat(bus.getQueueSize(), is(10));
+		Thread.sleep(600);
+		assertThat(bus.getQueueSize(), is(5));
+		Thread.sleep(500);
+		assertThat(bus.getQueueSize(), is(0));
+	}
+}
---
+++ src/test/java/de/gzockoll/observation/InstrumentConfigurationTest.java
@@ -0,0 +1,35 @@
+/**
+ * Created 08.02.2012
+ * This code is copyright (c) 2004 PAYONE Gmbh & Co. KG.
+ */
+package de.gzockoll.observation;
+
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import org.junit.Test;
+
+import de.gzockoll.measurement.InstrumentConfiguration;
+
+/**
+ * @author Guido Zockoll
+ * 
+ */
+public class InstrumentConfigurationTest {
+
+	/**
+	 * Test method for
+	 * {@link de.gzockoll.prototype.camel.measurement.InstrumentConfiguration#builder()}
+	 * .
+	 */
+	@Test
+	public void testBuilder() {
+		InstrumentConfiguration conf = InstrumentConfiguration.builder()
+				.name("JUnit").unit("m/s").build();
+
+		String s = conf.toString();
+		assertThat(s, containsString("name=JUnit"));
+		assertThat(s, containsString("unit=m/s"));
+	}
+
+}
